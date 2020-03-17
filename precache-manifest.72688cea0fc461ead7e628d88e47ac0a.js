a97418e68fa55b64bad58ed4496d589bf67d56cd
eilin <edward.id.lin@gmail.com>
2020-03-17 09:48:17 -0400

    Updates

---
+++ precache-manifest.72688cea0fc461ead7e628d88e47ac0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff6651801191be28450a468803ae14c",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "65ba7fee4dbba4c6918b",
+    "url": "/resume/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "45c1de006d597986eaba",
+    "url": "/resume/static/js/2.c282922e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/resume/static/js/2.c282922e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "65ba7fee4dbba4c6918b",
+    "url": "/resume/static/js/main.47773b72.chunk.js"
+  },
+  {
+    "revision": "b9581d70d35e7fe98e77",
+    "url": "/resume/static/js/runtime-main.f694fd22.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/resume/static/media/logo.5d5d9eef.svg"
+  }
+]);
