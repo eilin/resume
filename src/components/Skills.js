942d3902da5b15e53cc3a61842fa75c48f56a957
eilin <edward.id.lin@gmail.com>
2020-03-18 13:27:25 -0400

    add resume skills data, basic skills listing

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react'
 
-function Skills() {
-	return (
-		<div>
-			<h2>Skills</h2>
-			<p>Sed sem odio, consequat ac urna in, bibendum tincidunt libero. Praesent vitae auctor neque. Vestibulum at orci posuere, posuere sem vitae, auctor ligula. Maecenas et tincidunt turpis. Quisque sit amet quam id ipsum viverra auctor. Nulla mattis nulla vel purus dignissim fringilla. Donec sollicitudin nulla vel enim pulvinar, ac porta lacus rhoncus. Nulla ultricies sem eget dolor iaculis aliquam. Aliquam sed orci vulputate, semper metus eget, consequat felis.</p>
-		</div>
-	);
+class Skills extends Component {
+	state = {
+		groupBy: 'techLabel'
+	}
+
+	render() {
+		const resumeData = require('../data/resume.json');
+		const skills = resumeData.skills;
+		let languages = skills.filter((skill) => skill.techLabel === 'language');
+		languages.sort((a, b) => a.name < b.name ? -1 : 1)
+		const listItems = languages.map((skill) =>
+			<li>{skill.name}</li>
+		);
+		return (
+			<>
+				<h3>Programming Languages</h3>
+				<ul>{listItems}</ul>
+			</>
+		);
+	}
 }
 
 export default Skills;
