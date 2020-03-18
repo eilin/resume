306f98955b261639ef19993c275385d0d74bead6
eilin <edward.id.lin@gmail.com>
2020-03-18 15:47:41 -0400

    button to switch skill groupingwq

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
+import { Button, ButtonGroup } from 'react-bootstrap';
 import SkillGroup from './SkillGroup';
 
 class Skills extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			groupBy: 'level',
+			groupBy: 'techLabel',
 			resumeData: require('../../data/resume.json'),
 			labelMap: require('../../data/labelMap.json')
 		}
+	}
+
+	switchGroup(group) {
+		this.setState({
+			groupBy: group
+		});
+	}
+
+	buttonVariant(selfLabel) {
+		return this.state.groupBy === selfLabel ? 'primary' : 'outline-secondary';
 	}
 
 	render() {
 		let labels = this.state.labelMap[this.state.groupBy];
 		const skills = this.state.resumeData.skills;
 		let skillGroups = [];
-		for (const [key,value] of Object.entries(labels)) {
+		for (const [key, value] of Object.entries(labels)) {
 			skillGroups.push(<SkillGroup key={key} skills={skills} groupBy={this.state.groupBy} groupKey={key} label={value} />);
 		}
 
 		return (
 			<>
+				<ButtonGroup aria-label="Basic example">
+					<Button variant={this.buttonVariant("techLabel")} onClick={() => this.switchGroup("techLabel")}>Technology</Button>
+					<Button variant={this.buttonVariant("level")} onClick={() => this.switchGroup("level")}>Proficiency</Button>
+				</ButtonGroup>
 				{skillGroups}
 			</>
 		);
